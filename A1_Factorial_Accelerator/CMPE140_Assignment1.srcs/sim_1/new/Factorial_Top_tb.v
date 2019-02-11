72883e29d4ab430883e9f7b1c546952b9389cd5e
nickschiffer <nick@schiffer.us>
2019-02-11 12:51:47 -0800

    Improved Top_tb

--- A1_Factorial_Accelerator/CMPE140_Assignment1.srcs/sim_1/new/Factorial_Top_tb.v
+++ A1_Factorial_Accelerator/CMPE140_Assignment1.srcs/sim_1/new/Factorial_Top_tb.v
@@ -27,6 +27,7 @@ reg Go_tb, clk_tb;
 reg [3:0] n_tb;
 wire Done_tb, Err_tb;
 wire [31:0] nf_tb;
+reg [31:0] nf_inf;
 
 Factorial_Top DUT (
     .n(n_tb),
@@ -47,6 +48,21 @@ task automatic tick;
     end
 endtask
 
+function automatic [31:0] factorial;
+    input [3:0]n;
+    reg [31:0]nl, nf;
+    begin
+    nl = n;
+    nf = 1;
+    while (nl > 1)
+    begin
+        nf = nf * nl;
+        nl = nl - 1;
+    end
+    factorial = nf;
+    end
+endfunction
+
 initial
 begin
     $display("Factorial Top Test Begin");
@@ -62,7 +78,23 @@ begin
         begin
             tick;
         end
-        $display("%d! = %d",n_tb, nf_tb);
+        if (Done_tb)
+        begin
+        $display("%0d!     = %0d",n_tb, nf_tb);
+        nf_inf = factorial(n_tb);
+        $display("%0d! inf = %0d\n",n_tb, nf_inf);
+        end
+        else if (Err_tb)
+        begin
+            $display("Error received, input = %0d", n_tb);
+        end
+        if ((nf_tb != nf_inf) && (n_tb <= 12))
+        begin
+            $display("Error: got %0d, expected %0d for $0d!",nf_tb, nf_inf, n_tb);
+            $stop;
+        end
+        
+        
         n_tb = n_tb + 4'd1;
     end
     
