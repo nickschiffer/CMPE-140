df931cb087d03a0148c604610c4841384b7a5193
nickschiffer <nick@schiffer.us>
2019-03-19 20:56:45 -0700

    Device Works

--- A6_Processor_Hardware_Validation/Processor_Hardware_Validation/Processor_Hardware_Validation.srcs/sources_1/imports/basys3_single_cycle_mips_validation/hex_to_7seg.v
+++ A6_Processor_Hardware_Validation/Processor_Hardware_Validation/Processor_Hardware_Validation.srcs/sources_1/imports/basys3_single_cycle_mips_validation/hex_to_7seg.v
@@ -1,28 +1,28 @@
-module hex_to_7seg (
-            input  wire [3:0] HEX,
-            output reg  [7:0] s
-        );
-
-    always @ (HEX) begin
-        case (HEX) 
-               4'h0: s = 8'b11000000;
-               4'h1: s = 8'b11111001;
-               4'h2: s = 8'b10100100;
-               4'h3: s = 8'b10110000;
-               4'h4: s = 8'b10011001;
-               4'h5: s = 8'b10010010;
-               4'h6: s = 8'b10000010;
-               4'h7: s = 8'b11111000;
-               4'h8: s = 8'b10000000;
-               4'h9: s = 8'b10010000;
-               4'hA: s = 8'b10001000;
-               4'hB: s = 8'b10000000;
-               4'hC: s = 8'b11000110;
-               4'hD: s = 8'b11000000;
-               4'hE: s = 8'b10000110;
-               4'hF: s = 8'b10001110;
-            default: s = 8'b01111111;
-       endcase
-    end
-
+module hex_to_7seg
+(input [3:0] HEX, output reg [7:0] s);
+        always @ (HEX)
+        begin
+                case (HEX)
+                        0:      s = 8'b10001000;
+                        1:      s = 8'b11101101;
+                        2:      s = 8'b10100010;
+                        3:      s = 8'b10100100;
+                        4:      s = 8'b11000101;
+                        5:      s = 8'b10010100;
+                        6:      s = 8'b10010000;
+                        7:      s = 8'b10101101;
+                        8:      s = 8'b10000000;
+                        9:      s = 8'b10000100;
+                        /*
+                        TODO: Verify that these are correct
+                        */
+                        4'hA:   s = 8'b10000001;
+                        4'hB:   s = 8'b11010000;
+                        4'hC:   s = 8'b10011010;
+                        4'hD:   s = 8'b11100000;
+                        4'hE:   s = 8'b10010010;
+                        4'hF:   s = 8'b10010011;
+                        default:s = 8'b01111111;
+                endcase
+        end
 endmodule
